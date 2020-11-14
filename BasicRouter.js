7d4ed5b441928b99370af671b4c418a22693ffe6
DOM <pducduy99@gmail.com>
2020-11-14 12:23:54 +0700

    c

--- BasicRouter.js
+++ BasicRouter.js
@@ -4,21 +4,55 @@ const path = require('path');
 const router = express.Router();
 var fs = require('fs');
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-  
-  router.get('/data',function(req,res){
-  
-    var data = fs.readFileSync('data.txt');
-    var array = data.toString().split('\n');
-    res.send(array);
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/data', function (req, res) {
+  var data = fs.readFileSync('data.txt');
+  var array = data.toString().split('\n');
+  res.send(array);
+});
+
+router.get('/start', function (req, res) {
+  var date = new Date("dd/MM/yyyy");
+  var str = date.getDay().toString();
+  res.send("sdfsafdf");
+});
+
+// create helper middleware so we can reuse server-sent events
+const useServerSentEventsMiddleware = (req, res, next) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  // only if you want anyone to access this endpoint
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.flushHeaders();
+
+  const sendEventStreamData = (data) => {
+      const sseFormattedResponse = `data: ${JSON.stringify(data)}\n\n`;
+      res.write(sseFormattedResponse);
+  }
+
+  // we are attaching sendEventStreamData to res, so we can use it later
+  Object.assign(res, {
+      sendEventStreamData
   });
 
-  //add the router
-  app.use('/', router);
-  app.listen(process.env.port || 3000);
-  
-  console.log('Running at Port 3000');
+  next();
+}
+
+const startTime = (req, res) => {
+      res.write("hello");
+}
+
+app.get('/start', useServerSentEventsMiddleware, 
+startTime)
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
 
