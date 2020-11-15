40dd44685b86fb949343cded64eb1029da6347cb
Laptop-DOM <pducduy99@gmail.com>
2020-11-15 12:15:45 +0700

    lời nhắc

--- BasicRouter.js
+++ BasicRouter.js
@@ -13,18 +13,22 @@ router.get('/', function (req, res) {
 router.get('/data', function (req, res) {
   var data = fs.readFileSync('data.txt');
   var array = data.toString().split('\n');
-  res.send(array);
+  if(data.length != 0) res.send(array);
 });
-
 
 router.get('/start', function (req, res) {
   var day = new Date().getDate();
-  var month =  new Date().getMonth();
+  var month =  new Date().getMonth() + 1;
   var year =  new Date().getFullYear();
   var date = day + "/" + month + "/" + year;
   var hours = new Date().getHours();
   var minutes = new Date().getMinutes();
   var seconds = new Date().getSeconds();
+  if (minutes < 10) minutes = "0" + minutes;
+  if (hours < 10) hours = "0" + hours;
+  if (seconds < 10) seconds = "0" + seconds;
+  if (day < 10) day = "0" + day;
+  if (month < 10) month = "0" + month;
 
   var writeFile = date + " " + hours + ":" + minutes + ":" + seconds + " " + "null" + " " + "null" + " " + "start";
 
@@ -32,7 +36,28 @@ router.get('/start', function (req, res) {
     if (error)
         console.error(error.message);
   });
+
   res.redirect('/');
+});
+
+router.get('/end', function (req, res) {
+
+  var endTime = req.query.endTime;
+  var totalTime = req.query.totalTime;
+
+  var data = fs.readFileSync('data.txt');
+  var array = data.toString().split('\n');
+
+  array[0] =  array[0].replace('null', endTime);
+  array[0] =  array[0].replace('null', totalTime);
+  array[0] =  array[0].replace('start', 'end');
+  var str="";
+  for( i of array){
+   str += i + "\n";
+  };
+  str = str.trim();
+  fs.writeFileSync('data.txt',str);
+  res.redirect('/'); 
 });
 
 //add the router
